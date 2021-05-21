2bb9ac0c90e83b005b42c2256c44fa1a1ba4b5e3
unknown <fazlaycse@yahoo.com>
2021-05-21 16:22:23 +0600

    one_yr_certificate data fetched

--- resources/views/technical/tecStdFirstPage.blade.php
+++ resources/views/technical/tecStdFirstPage.blade.php
@@ -54,7 +54,7 @@
                 <div class="input-group-prepend">
                     <span class="input-group-text bg-number">২.৮</span>
                 </div>
-                <div class="form-control bg-number-label">এসএসসি/দাখিল ভোকেশনাল এর শ্রেণিভিত্তিক শিক্ষার্থী তথ্য ২০২১
+                <div class="form-control bg-number-label">এসএসসি ভোকেশনাল এর শ্রেণিভিত্তিক শিক্ষার্থী তথ্য ২০২১
                 </div>
             </div>
             <div class="contentBoxBody">
@@ -111,7 +111,7 @@
                 <div class="input-group-prepend">
                     <span class="input-group-text bg-number">২.৯</span>
                 </div>
-                <div class="form-control bg-number-label">এইচএসসি/আলিম ভোকেশনাল এর বর্ষভিত্তিক শিক্ষার্থী তথ্য ২০২১</div>
+                <div class="form-control bg-number-label">এইচএসসি ভোকেশনাল এর বর্ষভিত্তিক শিক্ষার্থী তথ্য ২০২১</div>
             </div>
             <div class="contentBoxBody">
                 <table class="table table-bordered table-striped text-center">
@@ -164,7 +164,7 @@
                 <div class="input-group-prepend">
                     <span class="input-group-text bg-number">২.১০</span>
                 </div>
-                <div class="form-control bg-number-label">এইচএসসি/আলিম বিএম এর বর্ষভিত্তিক শিক্ষার্থী তথ্য ২০২১</div>
+                <div class="form-control bg-number-label">এইচএসসি বিএম এর বর্ষভিত্তিক শিক্ষার্থী তথ্য ২০২১</div>
             </div>
             <div class="contentBoxBody">
                 <table class="table table-bordered table-striped text-center">
@@ -222,56 +222,47 @@
             </div>
             <div class="contentBoxBody">
                 <table class="table table-bordered table-striped text-center">
-
+                    <thead>
                     <tr>
-                        {{--<td>স্তর</td>--}}
                         <td rowspan="2">শ্রেণি</td>
-
                         <td colspan="2">শিক্ষার্থীর সংখ্যা</td>
                         <td colspan="2">উপবৃত্তি প্রাপ্ত শিক্ষার্থীর সংখ্যা</td>
-                        <td colspan="2">উপস্থিতি(১৫ জুলাই ২০২১)</td>
-                        <td colspan="2">পুনঃভর্তিকৃত(রিপিটার)শিক্ষার্থীর সংখ্যা</td>
-                        <td rowspan="2" >ট্রান্সফার ইন</td>
+                        <td colspan="2">মেধাবৃত্তি প্রাপ্ত শিক্ষার্থীর সংখ্যা</td>
+                        <td colspan="2">তথ্য প্রদানের দিন উপস্থিত শিক্ষার্থীর সংখ্যা</td>
+                        <td colspan="2">রিপিটার শিক্ষার্থীর সংখ্যা</td>
+                        <td rowspan="2">ট্রান্সফার ইন</td>
                         <td rowspan="2">ট্রান্সফার আউট</td>
-                        <td rowspan="2">২০২২ এর পাঠ্য পুস্তকের চাহিদা অনুযায়ী শিক্ষার্থীর সংখ্যা</td>
+
                     </tr>
                     <tr>
                         <td>মোট</td>
                         <td>ছাত্রী</td>
                         <td>মোট</td>
                         <td>ছাত্রী</td>
-                        <td>ছাত্র</td>
+                        <td>মোট</td>
                         <td>ছাত্রী</td>
-                        <td>ছাত্র</td>
+                        <td>মোট</td>
+                        <td>ছাত্রী</td>
+                        <td>মোট</td>
                         <td>ছাত্রী</td>
 
                     </tr>
-                    </tdead>
-                    <tbody ng-repeat="item in data.classes">
-                    <td rowspan="@{{item.groups.length+1}}">@{{item.class_name_bangla}}</td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].total_student"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].female_student"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].male_stipend"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].female_stipend"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].male_scholarship"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].female_scholarship"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].total_present"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].female_present"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].transfer_in"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].transfer_out"/></td>
-                    <td ng-if="item.groups.length==0"><input type="number" number-converter class="w-50" ng-init="idy = findClIndex(item.class_id,item.education_level_id)" ng-model="studentSummery[idy].nextyr_book_reg"/></td>
-                    <tr ng-repeat="i in item.groups" ng-if="item.groups.length!=0">
-                        <td>@{{i.group_name_bn}}</td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].female_student"/></td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].male_stipend"/></td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].female_stipend"/></td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].male_scholarship"/></td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].female_scholarship"/></td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].total_present"/></td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].female_present"/></td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].transfer_in"/></td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].transfer_out"/></td>
-                        <td><input type="number" number-converter class="w-50" ng-model="studentSummery[idx].nextyr_book_reg"/></td>
+                    </thead>
+                    <tbody>
+                    <tr ng-repeat="item in data.one_yr_certificate">
+                        <td ng-bind="one_yr_certificate(item.class_id)"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.total_student"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.female_student"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.male_stipend"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.female_stipend"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.male_scholarship"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.female_scholarship"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.total_present"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.female_present"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.total_rep"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.female_rep"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.transfer_in"></td>
+                        <td><input type="number" number-converter class="w-50"ng-model="item.transfer_out"></td>
                     </tr>
                     </tbody>
                 </table>
