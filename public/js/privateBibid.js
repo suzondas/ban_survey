cdd24beb09836be1ea5ea5955ba77381145be892
ifti1989 <mr.ifti.cse@gmail.com>
2021-07-05 17:28:55 +0600

    private uni js

---
+++ public/js/privateBibid.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el: '#privateBibid',
+    data() {
+        return {
+            data: [],
+            dataLoaded: true,
+            dataLoadingError: true
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+});
+
+
