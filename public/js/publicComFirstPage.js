eea2702af8dfae3db3f4a74438aed1498c3b0b8e
Suzon Das <aanmehedi@gmail.com>
2021-07-20 13:59:39 +0600

    added Add more on first page with modal and full functionalities @publicComFirstPage

--- public/js/publicComFirstPage.js
+++ public/js/publicComFirstPage.js
@@ -1,10 +1,18 @@
+/*Registering Modal for Add more for showAddForeignUnivModal*/
+
 const app = new Vue({
     el: '#publicComFirstPage',
+    components: {
+        "modal": {
+            template: "#modal-showAddForeignUnivModal",
+        }
+    },
     data() {
         return {
             data: null,
             dataLoaded: false,
-            dataLoadingError: false
+            dataLoadingError: false,
+            showAddForeignUnivModal: false,
         }
     },
     mounted() {
@@ -27,8 +35,8 @@ const app = new Vue({
         submitData: function () {
             var dataToSend = {};
             dataToSend.instId = inst_id;
-            dataToSend.institutes=this.data.institutes;
-            dataToSend.foreign_univ_institutes=this.data.foreign_univ_institutes;
+            dataToSend.institutes = this.data.institutes;
+            dataToSend.foreign_univ_institutes = this.data.foreign_univ_institutes;
             console.log(dataToSend);
             // return;
             axios.post('http://127.0.0.1:8000/publicComFirstPage/submitData/', dataToSend)
@@ -42,19 +50,39 @@ const app = new Vue({
                         alert("Error Try again");
                     });
         },
-        addMoreForeignUniv:function(){
-            this.data.foreign_univ_institutes.push(
-                {
-                    "id": null,
-                    "institute_id":inst_id,
-                    "sl_no": null,
-                    "univ_name": "",
-                    "country_name": "",
-                    "year": null,
-                    "created_at": null,
-                    "updated_at": null
-                }
-            )
+        addMoreForeignUniv: function () {
+            var self = this;
+            var toSend = {};
+            toSend.institute_id = inst_id;
+            toSend.univ_name = $("#univ_name").val();
+            toSend.country_name = $("#country_name").val();
+            axios.post('http://127.0.0.1:8000/publicComFirstPage/addForeignUnivInst/', toSend)
+                .then(
+                    function (response) {
+                        self.data.foreign_univ_institutes.push(response.data);
+                        $("#univ_name").val("");
+                        $("#country_name").val("");
+                        self.showAddForeignUnivModal = false;
+                        alert("Item Added successfully");
+                    },
+                    function (response) {
+                        alert("Error Try again");
+                    });
+
+        },
+        deleteForeignUniv: function (id, index) {
+            /*console.log(id)
+            console.log(index);return;*/
+            var self = this;
+            axios.get('http://127.0.0.1:8000/publicComFirstPage/removeForeignUnivInst/' + id)
+                .then(
+                    function (response) {
+                        self.data.foreign_univ_institutes.splice(index, 1)
+                        alert("Item removed successfully");
+                    },
+                    function (response) {
+                        alert("Error Try again");
+                    });
         }
     }
 });
